14c253a92d4891818e5e7ea83b871093aaf82f67
INTERNAL\rokas.cvirka <rokas.cvirka@visma.com>
2023-03-30 13:02:40 +0300

    Added basic menu logic

--- ConversionsExercise/Program.cs
+++ ConversionsExercise/Program.cs
@@ -4,23 +4,84 @@
     {
         static void Main(string[] args)
         {
+            bool isRunning = true;
             
-            while(true)
+            while(isRunning)
             {
-                Console.WriteLine("Įveskite skaičių: ");
+                string input = GetNumberFromUser();
+                if (input == "QUIT") 
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    int number = int.Parse(input);
 
-                int number = int.Parse(Console.ReadLine());
-                string hexadecimal = ConvertToBase(number, 16);
-                string oct = ConvertToBase(number, 8);
-                string binary = ConvertToBase(number, 2);
-                Console.WriteLine("Hexadecimal: " + hexadecimal);
-                Console.WriteLine("Oct: " + oct);
-                Console.WriteLine("Binary: " + binary);
-                Console.WriteLine();
+                    var options = DisplayOptions(number);
+                    switch (options)
+                    {
+                        case "1":
+                            string binary = ConvertToBase(number, 2);
+                            Console.WriteLine("Binary: " + binary);
+                            break;
+                        case "2":
+                            string oct = ConvertToBase(number, 8);
+                            Console.WriteLine("Oct: " + oct);
+                            break;
+                        case "3":
+                            string hexadecimal = ConvertToBase(number, 16);
+                            Console.WriteLine("Hexadecimal: " + hexadecimal);
+                            break;
+                        case "QUIT":
+                            isRunning = false;
+                            break;
+
+                    }
+                    Console.WriteLine();
+                }
             }
 
         }
 
+        public static string GetNumberFromUser()
+        {
+            Console.WriteLine("Įveskite skaičių, kurį norite konvertuoti: ");
+            string input = Console.ReadLine();
+            if (input == "QUIT")
+            {
+                return "QUIT";
+            }
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Įvestis negali būti tuščia. Bandykite dar kartą...");
+                return GetNumberFromUser();
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        public static string DisplayOptions(int number)
+        {
+            Console.WriteLine($"Skaičius {number}.{Environment.NewLine}Pasirinkite 1 - jeigu norite konvertuoti į binary,{Environment.NewLine}2 - jei norite konvertuoti į oct,{Environment.NewLine}3 - jeigu norite konvertuoti į hexadecimal,{Environment.NewLine}4 - išjungti programą.");
+            var option = Console.ReadLine();
+            if(IsNotEmpty(option, number))
+            {
+                return option;
+            }
+            return "Įvestis negali būti tuščia";
+        }
+
+        public static bool IsNotEmpty(string input, int number)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Įvestis negali būti tuščia. Bandykite dar kartą...");
+                DisplayOptions(number);
+            }
+            return true;
+        }
         public static string ConvertToBase(int decimalNumber, int baseNumber)
         {
             const string DIGITS = "0123456789ABCDEF";
@@ -39,6 +100,15 @@
             }
 
             return result;
+        }
+
+        public static bool isOptionInvalid(string option, string[] validOptions)
+        {
+            if (validOptions.Contains(option))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
